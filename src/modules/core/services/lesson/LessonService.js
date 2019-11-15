bc5d0c205874388df89f6551695f42cfbedc1675
J. Daza <blackencio33@gmail.com>
2019-11-14 20:32:47 -0400

    add services

---
+++ src/modules/core/services/lesson/LessonService.js
@@ -0,0 +1,21 @@
+import Base from "../Base";
+
+const resource = "/lessons";
+
+export default {
+    create(payload) {
+        return Base.post(`${resource}/create`, payload);
+    },
+    get(id) {
+        return Base.get(`${resource}/${id}`);
+    },
+    delete(id) {
+        return Base.delete(`${resource}/${id}`);
+    },
+    edit(id, changes) {
+        return Base.put(`${resource}/${id}`, changes);
+    },
+    list(filters = {}) {
+        return Base.post(`${resource}/list`, filters);
+    }
+};
